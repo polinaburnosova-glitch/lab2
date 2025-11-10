7e37f71bddd763cafed64c779f7f4f64b9cdce26
Полина <polinaburnosova@gmail.com>
2025-11-10 19:26:10 +0300

    Исправлены имена

--- src/Lab2.java
+++ src/Lab2.java
@@ -3,8 +3,8 @@ import Pokes.*;
 public class Lab2 {
     public static void main(String arg [] ){
         Battle b = new Battle();
-        Pokemon[] allies = { new Carbink("Блум", 19), new Charjabug("Стелла", 20), new Cinccino("Флора", 15) };
-        Pokemon[] foes = { new Grubbin("Айси", 17), new Minccino("Дарси", 19), new Vikavolt("Трикси", 20) };
+        Pokemon[] allies = { new Carbink("Bloom", 19), new Charjabug("Stella", 20), new Cinccino("Flora", 15) };
+        Pokemon[] foes = { new Grubbin("Icy", 17), new Minccino("Darcy", 19), new Vikavolt("Stromy", 20) };
         for (Pokemon p : allies)
             b.addAlly(p);
         for (Pokemon f : foes)
